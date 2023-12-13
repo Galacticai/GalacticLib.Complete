5d7cc3b5c049d852ac8a4d4815088362be388f2a
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-12-13 16:31:28 +0200

    feat: tonna stuff

--- Objects/FutureValue.cs
+++ Objects/FutureValue.cs
@@ -1,9 +1,29 @@
 
+using GLib;
+
 namespace GalacticLib.Objects;
 
-public record FutureValue<TValue> {
-    public sealed record Finished(TValue Value) : FutureValue<TValue>;
+public abstract record FutureValue<TValue> {
+
+    /// <summary> Value is pending to be gotten </summary>
     public sealed record Pending : FutureValue<TValue>;
-    public sealed record TimedOut(int Duration) : FutureValue<TValue>;
-    public sealed record NotFound : FutureValue<TValue>;
+
+    /// <summary> Running to get the value </summary>
+    public sealed record Running : FutureValue<TValue>;
+
+    /// <summary> <see cref="Value"/> was aquired </summary>
+    /// <param name="Value"> The value that was aquired (could also be <see langword="default"/>) </param>
+    public sealed record Finished(TValue? Value) : FutureValue<TValue>;
+
+    /// <summary> Something went wrong while getting the value </summary>
+    public abstract record Failed : FutureValue<TValue> {
+        /// <summary> Failed to get the value due to <paramref name="Duration"/> passing and invalidating the process </summary>
+        /// <param name="Duration"> Time elapsed while trying to get the value </param>
+        public sealed record TimedOut(int Duration) : Failed;
+
+        /// <summary> Failed to get the value due to an <paramref name="Exception"/> </summary>
+        /// <param name="Exception"> <see cref="Exception"/> encountered while getting the value </param>
+        public sealed record Error(Exception Exception) : Failed;
+    }
+
 }
