14f219d71019869f8faa12ee084cabda0f8592ec
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-12-18 16:36:13 +0200

    rename

--- Objects/DataStructure/SinglyLinkedListNode.cs
+++ Objects/DataStructure/SinglyLinkedListNode.cs
@@ -2,25 +2,25 @@ using System.Collections;
 
 namespace GalacticLib.Objects.DataStructure;
 
-public class LinkedList<TValue>(TValue value, LinkedList<TValue>? next = null)
+public class SinglyLinkedListNode<TValue>(TValue value, SinglyLinkedListNode<TValue>? next = null)
         : IEnumerable<TValue> {
     public TValue Value = value;
-    public LinkedList<TValue>? Next = next;
+    public SinglyLinkedListNode<TValue>? Next = next;
 
     public override string ToString()
         => $"[{string.Join(',', this)}]";
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator(); //? old... return modern one below
     public IEnumerator<TValue> GetEnumerator() {
-        LinkedList<TValue>? node = this;
+        SinglyLinkedListNode<TValue>? node = this;
         while (node is not null) {
             yield return node.Value;
             node = node.Next;
         }
     }
 
-    public static implicit operator List<TValue>(LinkedList<TValue> node)
+    public static implicit operator List<TValue>(SinglyLinkedListNode<TValue> node)
         => [.. node];
-    public static implicit operator TValue(LinkedList<TValue> node)
+    public static implicit operator TValue(SinglyLinkedListNode<TValue> node)
         => node.Value;
 }
