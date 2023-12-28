7f1ec947d6a3e0bd7398744b37e1cb8d034893c2
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-12-28 19:38:42 +0200

    move: ->Trees namespae

--- Objects/DataStructure/Trees/SinglyLinkedListNode.cs
+++ Objects/DataStructure/Trees/SinglyLinkedListNode.cs
@@ -2,8 +2,12 @@ using System.Collections;
 
 namespace GalacticLib.Objects.DataStructure;
 
-public class SinglyLinkedListNode<TValue>(TValue value, SinglyLinkedListNode<TValue>? next = null)
-        : IEnumerable<TValue> {
+public class SinglyLinkedListNode<TValue>(
+        TValue value,
+        SinglyLinkedListNode<TValue>? next = null
+
+) : IEnumerable<TValue> {
+
     public TValue Value = value;
     public SinglyLinkedListNode<TValue>? Next = next;
 
