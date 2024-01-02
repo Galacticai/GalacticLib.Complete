82c21d0f46cd92f88f322dec17f7911de5ff2c8d
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-01-03 00:55:53 +0200

    feat: WordDictionary

---
+++ Objects/DataStructure/WordDictionary.cs
@@ -0,0 +1,16 @@
+using GalacticLib.Objects.DataStructure.Trees.NaryTrees;
+
+namespace GalacticLib.Objects.DataStructure;
+
+public class WordDictionary() : NumberNaryTreeNode<char>('\x02') {
+    /// <summary> Check the existence of a sentence</summary>
+    /// <returns> true if the whole sentence was found </returns>
+    public bool Contains(IEnumerable<IEnumerable<char>> sentence) {
+        foreach (var word in sentence)
+            if (!base.Contains(word, out _))
+                return false;
+        return true;
+    }
+    public bool Contains(params string[] sentence)
+        => Contains(sentence as IEnumerable<IEnumerable<char>>);
+}
